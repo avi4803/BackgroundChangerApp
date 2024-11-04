260b47021a92119d964e38e709374456b99698b6
Avinash Nishad <avinashn157@outlook.com>
2024-11-04 14:01:52 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+  SafeAreaInsetsContext,
+  useSafeAreaInsets,
+} from "react-native-safe-area-context";
 
-export default function App() {
+import {
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from "react-native";
+
+function App() {
+  const [randomBackground,setrandomBackground] = useState("#C6E7FF");
+  const colorGenerator = ()=>{
+    const hexRange = "0123456789ABCDEF"
+    let color = "#"
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random()*16)];
+      
+    }
+    setrandomBackground(color);
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.outerView, {backgroundColor:randomBackground}} >
+      <SafeAreaView>
+        <View style={[styles.Container]}>
+          <TouchableOpacity onPress={colorGenerator} >
+            <View style={[styles.actionBtn]}>
+              <Text style={[styles.buttonText]}>Click Me</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     </View>
   );
-}
+} 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Container: {
+    justify:"center",
+    alignItems: "center",
+    height:"100%"
+   
+  },
+  actionBtn: {
+    marginTop:350,
+    borderRadius: 5,
+    backgroundColor: "#CB9DF0",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+  },
+
+  outerView: {
+  
+
+
+  },
+  buttonText: {
+    fontSize: 20,
+    fontWeight: "thin",
   },
 });
+
+export default App;
